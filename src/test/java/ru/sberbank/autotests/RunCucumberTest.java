07564387411cacad5362be5d48c3edfa7511ade0
Susclick <sus@aknet.ru>
2020-04-02 21:20:27 +0300

    Добавил конфиг для запуска из-под мавена + кодировку для нормального вывода в консоь

--- src/test/java/ru/sberbank/autotests/RunCucumberTest.java
+++ src/test/java/ru/sberbank/autotests/RunCucumberTest.java
@@ -5,6 +5,8 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = {"pretty"},
+        features = "src/test/resources/features",
+        glue = "ru.sberbank.autotests.glue")
 public class RunCucumberTest {
 }
