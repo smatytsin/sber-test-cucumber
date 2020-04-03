1dbcf6b9867abfd7665c3ab4389f6719cb85b596
Susclick <sus@aknet.ru>
2020-04-04 00:54:34 +0300

    1. Добавил плагин аллюра в класс

--- src/test/java/ru/sberbank/autotests/RunCucumberTest.java
+++ src/test/java/ru/sberbank/autotests/RunCucumberTest.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"},
+@CucumberOptions(plugin = {"pretty", "io.qameta.allure.cucumber4jvm.AllureCucumber4Jvm"},
         features = "src/test/resources/features",
         glue = "ru.sberbank.autotests.glue")
 public class RunCucumberTest {
